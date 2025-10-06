5daeac7851a4776b3b04c7fd7e309d0a008012a6
devnizamuddin <dev.nizamuddin@gmail.com>
2025-10-06 12:16:40 +0600

    🎨 Style: main.dart code seprated and unnessary comment removed

---
+++ lib/development_kit_app.dart
@@ -0,0 +1,17 @@
+import 'package:development_kit_flutter/home_page.dart';
+import 'package:flutter/material.dart';
+
+class DevelopmentKitApp extends StatelessWidget {
+  const DevelopmentKitApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Development Kit ',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: const HomePage(title: 'Home Page'),
+    );
+  }
+}
