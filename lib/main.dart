f035b37a3e5d37f3d08d8aafc64326f22da873fe
devnizamuddin <dev.nizamuddin@gmail.com>
2025-10-16 21:37:26 +0600

    ✨ Feat: error folder added

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,10 @@
+import 'package:ansicolor/ansicolor.dart';
 import 'package:flutter/material.dart';
 
 import 'development_kit_app.dart';
 
 void main() {
+  ansiColorDisabled = false;
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(const DevelopmentKitApp());
 }
-
-
